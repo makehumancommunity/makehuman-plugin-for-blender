4fb753a97ebdf332f42e5ba8179edaa092c61ca3
jeff <jeff@198.105.254.228>
2016-07-18 15:27:34 -0400

    - implemented separate eyes - added doc-assist directory - did doc of eye operation
    More doc will follow.  You really need to see them in Github to know if
    they look right.
    
    Need to work elsewhere for a couple days though.

--- blender_source/MH_Community/README.md
+++ blender_source/MH_Community/README.md
@@ -1,11 +1,33 @@
 # MH Community 
 
-This is a blender plugin which brings features related to MakeHuman
+This is a blender plugin which brings features related to MakeHuman.  It provides for post import operations specific to MakeHuman meshes, and armatures.
+
+# Mesh based Operations
 
 ## Sync mesh
+
+##Separate Eyes & Center origins around mass
+|Eye Mesh Before| Eye Meshes After|
+| --- | --- 
+|<img src="doc-assist/eye_before.png">|<img src="doc-assist/eyes_after.png">
+
+The single Eye mesh as it comes out of MakeHuman is only really adjustable while using the Default (163 bones) or Default No Toes (137 bones) armatures.  In downstream systems this is way too many for some.  Android & iOS for example can barely manage around 23 using WebGL 1.0.
+
+There is another way to move them by just separating each eye, and assigning the same rotation.  Most gaming frameworks, including 'Blender Game', also have a feature called billboard mode.  When switched on, the framework ensures that a mesh is always facing the camera.  This allows for the effect of making the eyes follow the camera with very little work.
+
+It was observed that eyes also needed to be setback a minor amount to keep from violating the skin when rotation was large (15% eye depth for billboard mode & 10% for manual setting).  This is not really noticable, however piercing the skin most definitely is. 
+
+If managing the setting outside of billboard mode, staying between -0.12 and 0.12 radians seem to be the practical limits.  Left & right limits vary by eye.
+
+||Rotate Left| Rotate Right|
+|Left Eye|-0.50|0.35|
+|Right Eye|-0.35|0.50|
+
+# Armature Operations
 
 ## Sync pose
 
 ## IK
 
-## (etc...)
+
+
--- blender_source/MH_Community/separate_eyes.py
+++ blender_source/MH_Community/separate_eyes.py
@@ -1,5 +1,8 @@
 import bpy
 #===============================================================================
+HIPOLY_VERTS  = 1064
+LOWPOLY_VERTS = 96
+
 class SeparateEyes(bpy.types.Operator):
     """Separate The Eye mesh into left & right meshes, and move origin to center of mass of each."""
     bl_idname = 'mh_community.separate_eyes'
@@ -7,11 +10,60 @@ class SeparateEyes(bpy.types.Operator):
 
     bl_options = {'REGISTER', 'UNDO'}
 
-    def execute(self, context):        
+    def execute(self, context):     
+        combinedMesh = context.object
+        priorName = combinedMesh.name
+        nVertsHalf = len(combinedMesh.data.vertices) / 2
+        
+        # switch to edit, and de-select all verts Change selection mode
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='DESELECT')
+        bpy.context.tool_settings.mesh_select_mode = (True , False , False)
+        
+        #selection using code do not actually work unless in object mode
+        bpy.ops.object.mode_set(mode='OBJECT')
+        
+        # select the first half of  the vertices, which is the lest side
+        for vIndex, vert in enumerate(combinedMesh.data.vertices):
+           if vIndex == nVertsHalf: break
+           vert.select = True
+
+        # switch back to edit & separate, which leaves the right side mesh still selected
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.separate(type='SELECTED')
+                
+        # interface is in a strange state, combinedMesh is Right side, switch to Object mode, rename, & move origin
+        bpy.ops.object.mode_set(mode='OBJECT')
+        combinedMesh.name = priorName + '_R'
+        bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
+        
+        # set base class, and also ignore skeleton for BabylonJS export
+        combinedMesh.data.baseClass = 'BEING.EyeBall'
+        combinedMesh.data.ignoreSkeleton = True # now needs to be parented to Body; do that yourself
+        
+        # select left side, make active, rename, move origin, 
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.select_all(action='DESELECT')
+        left = bpy.data.objects[priorName + '.001']
+        left.select = True
+        context.scene.objects.active = left
+        left.name =  priorName + '_L'
+        bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
+        
+        # set base class, and also ignore skeleton for BabylonJS export
+        left.data.baseClass = 'BEING.EyeBall'
+        left.data.ignoreSkeleton = True  # now needs to be parented to Body; do that yourself
+
         return {'FINISHED'}
-    
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -           
     @classmethod
     def poll(cls, context):
         ob = context.object
-        return ob and ob.type == 'MESH'
-#===============================================================================
+        
+        # must be a mesh
+        if not ob or ob.type != 'MESH': 
+            return False
+        
+        # vertex count must match
+        nVerts = len(ob.data.vertices)
+        return nVerts == HIPOLY_VERTS or nVerts == LOWPOLY_VERTS
