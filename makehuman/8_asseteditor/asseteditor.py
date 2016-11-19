29ec9f1e806b30b2fbfc1d7c44c2bb0d10e69726
Joel Palmius <joepal1976@hotmail.com>
2016-11-19 10:53:05 +0100

    Adjust asset editor code to use MHAPI's asset parser

--- makehuman/8_asseteditor/asseteditor.py
+++ makehuman/8_asseteditor/asseteditor.py
@@ -30,6 +30,7 @@ import log
 import os
 import re
 import mh
+import copy
 
 from PyQt4 import QtGui
 from PyQt4 import QtCore
@@ -41,55 +42,6 @@ from core import G
 from codecs import open
 
 mhapi = gui3d.app.mhapi
-
-
-
-
-# The Basic asset class, provides a dictionary and some methods to modify it
-class TAssets(object):
-
-    def __init__(self):
-        self.assetData = {
-            "Category": u"",
-            "Name"    : u"" ,
-            "Author"  : u"Unknown",
-            "License" : u"",
-            "Z_Depth" : int(50),
-            "Material": u"",
-            "UUID"    : u"",
-            "Thumb"   : u"",
-            "Tags"    : set('')
-        }
-
-
-    def getkeys(self):
-        return self.assetData.keys()
-
-    def getval(self, key):
-        return self.assetData[key]
-
-
-    def setval(self, key, val):
-        self.assetData[key] = val
-
-    def tagstostr(self):
-        tagstr = ""
-        for i in self.assetData["Tags"]:
-            tagstr += i + " "
-        return tagstr.strip()
-
-    def strtotags(self, tagstr, update = False):
-    #CAVE: Use with caution, not thoroughly tested
-        tagset = set('')
-        for i in tagstr.split():
-            tagset.add(i)
-        if update:
-            self.setval("Tags", tagset)
-        return tagset
-
-    def addtag(self, val=""):
-        self.assetData["Tags"].add(val)
-
 
 
 # The AssetEditor task:
@@ -108,14 +60,14 @@ class AssetEditorTaskView(gui3d.TaskView):
 
         self.notfound = mhapi.locations.getSystemDataPath("notfound.thumb")
 
+        self.asset = None
+        self.reset_asset = None
 
-        self.asset = TAssets()
-        self.reset_asset = TAssets()
         self.editkey = "Author"
 
 # selectBox contains asset type selector ...
 
-        self.selectBox = self.addLeftWidget(gui.GroupBox('Selct Asset Type'))
+        self.selectBox = self.addLeftWidget(gui.GroupBox('Select Asset Type'))
         self.selectBox.addWidget(gui.TextView("\nType:"))
 
         types = [ "Clothes",
@@ -134,13 +86,10 @@ class AssetEditorTaskView(gui3d.TaskView):
         self.selectBox.addWidget(gui.TextView("\nEdit:"))
 
         edittype  = [
-                    "Author",
-                    "Tags",
-                    "Z_Depth"
                      ]
 
-        self.typeList = mhapi.ui.createComboBox(edittype, self.onEditChange)
-        self.selectBox.addWidget(self.typeList)
+        self.tagList = mhapi.ui.createComboBox(edittype, self.onEditChange)
+        self.selectBox.addWidget(self.tagList)
 
         self.EditBox = self.addLeftWidget(gui.GroupBox("Edit: "))
         self.asset = self.AssetEditor(self.editkey, self.asset, self.reset_asset, caption=self.editkey, length=5)
@@ -156,27 +105,38 @@ class AssetEditorTaskView(gui3d.TaskView):
         self.mainPanel.setLayout(layout)
         self.addTopWidget(self.mainPanel)
 
+        self.assetInfoBox = gui.GroupBox("Asset info")
+        self.assetInfoText = self.assetInfoBox.addWidget(gui.TextView(""))
+        layout.addWidget(self.assetInfoBox)
+        self.set_assetInfoText(self.asset)
+
         self.assetThumbBox = gui.GroupBox("Asset thumbnail (if any)")
         self.thumbnail = self.assetThumbBox.addWidget(gui.TextView())
         self.thumbnail.setPixmap(QtGui.QPixmap(os.path.abspath(self.notfound)))
         self.thumbnail.setGeometry(0, 0, 128, 128)
         layout.addWidget(self.assetThumbBox)
 
-        self.assetInfoBox = gui.GroupBox("Asset info")
-        self.assetInfoText = self.assetInfoBox.addWidget(gui.TextView(""))
-        layout.addWidget(self.assetInfoBox)
-        self.set_assetInfoText(self.asset)
-
-
         @self.filechooser.mhEvent
         def onFileSelected(filename):
-            filename = mhapi.locations.getUnicodeAbsPath(filename)
-            self.asset = self.readAssetData(filename, self.selected_Type)
-            self.reset_asset.assetData = self.asset.assetData.copy()
-            self.thumbnail.setPixmap(QtGui.QPixmap(self.asset.getval("Thumb")))
+
+            assetInfo = mhapi.assets.openAssetFile(filename,True)
+            if assetInfo["thumb_path"]:
+                self.thumbnail.setPixmap(QtGui.QPixmap(assetInfo["thumb_path"]))
+            else:
+                self.thumbnail.setPixmap(QtGui.QPixmap(self.notfound))
             self.thumbnail.setGeometry(0,0,128,128)
+
+            self.asset = assetInfo
+            self.reset_asset = copy.deepcopy(assetInfo)
             self.set_assetInfoText(self.asset)
-            self.asset = self.AssetEditor(self.editkey, self.asset, self.reset_asset, caption=self.editkey, length=5)
+
+            self.tagList.clear()
+            
+            for k in assetInfo["pertinentKeys"]:
+                self.tagList.addItem(k)
+            for k in assetInfo["pertinentCommentKeys"]:
+                self.tagList.addItem(k)
+
 
 # Update filechooser on asset type:
     def onTypeChange(self, item=None):
@@ -216,26 +176,36 @@ class AssetEditorTaskView(gui3d.TaskView):
         self.editkey = str(item)
         self.asset = self.AssetEditor(self.editkey, self.asset, self.reset_asset, caption=self.editkey, length=5)
 
-
-
-
-    def AssetEditor(self, key=str, data=TAssets(), restoredata=TAssets(), caption = "", length=int(5)):
+    def AssetEditor(self, key=None, data=None, restoredata=None, caption = "", length=int(5)):
 
 
         for child in self.EditBox.children[:]:
             self.EditBox.removeWidget(child)
 
+        if not data:
+            print "No data"
+            return None
+
+        if not key in data:
+            print key + " is not in data"
+            return None
+
+        value = data[key]
+
+        if not value:
+            value = ""
+
+        keytype = type(value)
+
+        print keytype
+
         self.EditBox.addWidget(gui.TextView(caption))
 
-        keytype = type(data.getval(key))
-        if keytype is unicode:        # and suddenly, on load, somthing turns to unicode ... WTF...
-            keytype = type(str())
-
-        if keytype is str or keytype is int:
+        if keytype is str or keytype is int or keytype is unicode:
             if keytype is int:
-                Str_TextEditBox = self.EditBox.addWidget(qtgui.TextEdit(str(data.getval(key))))
-            if keytype is str:
-                Str_TextEditBox = self.EditBox.addWidget(qtgui.TextEdit(data.getval(key)))
+                Str_TextEditBox = self.EditBox.addWidget(qtgui.TextEdit(str(value)))
+            if keytype is str or keytype is unicode:
+                Str_TextEditBox = self.EditBox.addWidget(qtgui.TextEdit(value))
             Str_ActionBox = self.EditBox.addWidget(gui.GroupBox(""))
             Str_UButton = Str_ActionBox.addWidget(gui.Button('Update'))
             Str_RButton = Str_ActionBox.addWidget(gui.Button('Reset'))
@@ -259,7 +229,7 @@ class AssetEditorTaskView(gui3d.TaskView):
 
         if keytype is set:
 
-            itemlist = list(data.getval(key))
+            itemlist = list(data[key])
             if length < len(itemlist):
                 length = len(itemlist)
 
@@ -283,14 +253,14 @@ class AssetEditorTaskView(gui3d.TaskView):
                 if " " in change_set:
                     change_set.remove(" ")
 
-                data.setval(key, change_set)
+                data[key] = change_set
                 self.set_assetInfoText(data)
 
             @Set_RButton.mhEvent
             def onClicked(event):
 
-                data.setval(key, restoredata.getval(key))
-                ritemlist = list(data.getval(key))
+                data[key] = restoredata[key]
+                ritemlist = list(data[key])
                 for i in range(0, len(Set_TextEditBox) - len(ritemlist)):
                     ritemlist.append(" ")
                 i = 0
@@ -304,60 +274,6 @@ class AssetEditorTaskView(gui3d.TaskView):
 
 
 
-# Todo: Maybe the category shouldn't be set via readAssetData ...
-
-    def readAssetData(self, filename, assettype):
-        taglist = set()
-        asset_license = " "
-        u_name = " "
-        a_name = " "
-        default_material= " "
-        lasset = TAssets()
-
-        fobj = open(filename, 'r', encoding="utf-8")
-
-        for data in fobj.readlines():
-            linedata = data.split()
-            if len(linedata) > 0:
-                if linedata[0].lower() == "name":
-                    a_name = linedata[1]
-                if linedata[0].lower() == "tag":
-                    taglist.add(linedata[1].lower().capitalize())
-                if linedata[0].lower() == "z_depth":
-                    z_depth = int(linedata[1])
-                if linedata[0].lower() == "material":
-                    default_material = linedata[1]
-                if linedata[0].lower() == "uuid":
-                    uuid = linedata[1]
-                if len(linedata) >= 2 and linedata[0] == "#":
-                    if linedata[1].lower() == "license":
-                        for i in range(2, len(linedata)):
-                            asset_license += linedata[i] + " "
-                    if linedata[1].lower() == "author":
-                        for i in range(2, len(linedata)):
-                            u_name += linedata[i] + " "
-
-        fobj.close()
-
-        lasset.setval("License", asset_license.strip())
-        if u_name == " ":
-            u_name = "Unknown"
-        lasset.setval("User", u_name.strip())
-        if os.path.isfile(os.path.splitext(filename)[0] + ".thumb"):
-            thumb_file = os.path.splitext(filename)[0] + ".thumb"
-        else:
-            thumb_file = self.notfound
-        lasset.setval("Category", assettype)
-        lasset.setval("Thumb", thumb_file)
-        lasset.setval("Name", a_name)
-        lasset.setval("Author", u_name)
-        lasset.setval("Z_Depth", z_depth)
-        lasset.setval("Material", default_material)
-        lasset.setval("Tags", taglist)
-        lasset.setval("UUID", uuid)
-
-        return lasset
-
 #Todo: Implement asset file writter:
 
     def writeAssetData(self, filename, TAssets):
@@ -365,17 +281,24 @@ class AssetEditorTaskView(gui3d.TaskView):
 
 # The asset info window:
 
-    def set_assetInfoText(self, asset = TAssets()):
-
-        desc = "<big>" + asset.getval("Name") + "</big><br />\n&nbsp;<br />\n"
-        desc += "<b><tt>Category..........: </tt></b>" + asset.getval("Category") + "<br />\n"
-        desc += "<b><tt>Author............: </tt></b>" + asset.getval("Author") + "<br />\n"
-        desc += "<b><tt>Z_Depth...........: </tt></b>" + str(asset.getval("Z_Depth")) + "<br />\n"
-        desc += "<b><tt>Tags..............: </tt></b>" + asset.tagstostr() + "<br />\n"
-        desc += "<b><tt>UUID..............: </tt></b>" + asset.getval("UUID") + "<br />\n"
-        desc += "<b><tt>Default Material..: </tt></b>" + asset.getval("Material") + "<br />\n"
-        desc += "<b><tt>Thumb Path........: </tt></b>" + asset.getval("Thumb") + "<br />\n"
-        desc += "<b><tt>Asset License.....: </tt></b>" + asset.getval("License") + "<br />\n"
-
+    def set_assetInfoText(self, asset = None):
+        
+        if not asset:
+            desc = "<big>Nothing selected</big>"
+        else:
+            print asset
+            desc = "<big>" + asset["name"] + "</big><br />\n&nbsp;<br />\n"
+            for k in asset["pertinentCommentKeys"]:
+                value = asset[k]
+                if not value:
+                    value = "-"
+                desc += "<b><tt>" + k.ljust(15,".") + ": </tt></b>" + value + "<br />\n"
+            for k in asset["pertinentKeys"]:
+                if not k == "name":
+                    value = asset[k]
+                    if not value:
+                        value = "-"
+                    desc += "<b><tt>" + k.ljust(15,".") + ": </tt></b>" + value + "<br />\n"
+                
         self.assetInfoText.setText(desc)
 
