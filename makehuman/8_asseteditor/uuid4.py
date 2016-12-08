69d5eedd555532de9b272f6119784775dbbf9523
Aranuvir <Aranuvir@github>
2016-12-08 14:59:38 +0100

    better uuid generator code

--- makehuman/8_asseteditor/uuid4.py
+++ makehuman/8_asseteditor/uuid4.py
@@ -5,27 +5,18 @@
 #
 # License: Public Domain
 
-
-
 import random
 
-def get_charList():
-    charList=[]
-    for i in range(0,10):
-        charList.append(str(i))
-    for i in range(ord('a'), ord('f')+1):
-        charList.append(chr(i))
-    return charList
-
 def uuid4():
-    uuid_str=list("XXXXXXXX-XXXX-4XXX-YXXX-XXXXXXXXXXXX")
-    charList = get_charList()
-    x_charList = ['8','9','a','b']
+    uuid_str=list('XXXXXXXX-XXXX-4XXX-YXXX-XXXXXXXXXXXX')
+    hex_str= list('0123456789abcdef')
     random.seed()
     for i in range(36):
-        c = uuid_str[i]
-        if c == 'X':
-            uuid_str[i] = charList[random.randint(0, len(charList) - 1)]
-        if c == 'Y':
-            uuid_str[i] = x_charList[random.randint(0, len(x_charList) - 1)]
+        if uuid_str[i] == 'X':
+            uuid_str[i] = hex_str[random.randint(0, 15)]
+        elif uuid_str[i] == 'Y':
+            uuid_str[i] = hex_str[random.randint(8,11)]
     return str().join(uuid_str)
+
+for i in range(30):
+    print uuid4()
