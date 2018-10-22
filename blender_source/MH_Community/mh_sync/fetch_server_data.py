7f55867366eb01ecd47b117180d8c0d1363c22c5
Joel Palmius <joepal1976@hotmail.com>
2018-10-22 13:58:13 +0200

    Handle parameters

--- blender_source/MH_Community/mh_sync/fetch_server_data.py
+++ blender_source/MH_Community/mh_sync/fetch_server_data.py
@@ -1,11 +1,11 @@
 from .sync_ops import SyncOperator
 
 class FetchServerData(SyncOperator):
-    def __init__(self, functionName, readyFunction, expectBinary=False):
+    def __init__(self, functionName, readyFunction, expectBinary=False, params=None):
         super().__init__(functionName)
         self.readyFunction = readyFunction
         self.binary = expectBinary
-        self.executeJsonCall(expectBinaryResponse=expectBinary)
+        self.executeJsonCall(expectBinaryResponse=expectBinary, params=params)
 
     def callback(self, json_obj):
         if self.binary:
