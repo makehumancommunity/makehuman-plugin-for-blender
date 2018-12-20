240ccd6dabaf23426eb9dc9c9e0430dbe3380d7b
Joel Palmius <joepal1976@hotmail.com>
2018-12-20 15:21:39 +0100

    Utilities for various operations that differ between bl 27 and 28

---
+++ blender_source/MH_Community/util.py
@@ -0,0 +1,22 @@
+import bpy
+
+def bl28():
+    return bpy.app.version >= (2, 80, 0)
+
+def linkObject(obj):
+    if bl28():
+        bpy.context.collection.objects.link(obj)
+    else:
+        bpy.context.scene.objects.link(obj)
+
+def activateObject(obj):
+    if bl28():
+        bpy.context.view_layer.objects.active = obj
+    else:
+        bpy.context.scene.objects.active = obj
+
+def selectObject(obj):
+    if bl28():
+        obj.select_set(True)
+    else:
+        obj.select = True
