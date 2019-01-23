de246b9ab7331990322dfcd56c0611710496ebb0
Joel Palmius <joepal1976@hotmail.com>
2019-01-23 10:32:48 +0100

    Also build zip for blender 279 because the zfs%&jkgsrt¤%dfs blender approach for deciding compatibility of plugins doesn't allow us to use the same code for 280 and 279 eventhough the code as such works perfectly fine in both

--- rebuild_dist.bash
+++ rebuild_dist.bash
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 NAME=MH_Community
-ZIP=$NAME.zip
+ZNAME="${NAME}_for_blender"
+BLVER=280
+ZIP="${ZNAME}_${BLVER}.zip"
 DIR=`pwd`
 
 rm blender_distribution/$ZIP
 cd blender_source
 zip -r ../blender_distribution/$ZIP $NAME --exclude $NAME/\*__pycache__\* --exclude \*.pyc
+
+cp ../blender_distribution/$ZIP /tmp
+cd /tmp
+rm -rf MH_Community
+unzip $ZIP
+sed -i -e 's/"blender": (2, 80/"hejhopp": (2, 79/g' MH_Community/__init__.py
+BLVER=279
+ZIP="${ZNAME}_${BLVER}.zip"
+zip -r $ZIP $NAME
+
 cd $DIR
+cp /tmp/$ZIP blender_distribution
 
