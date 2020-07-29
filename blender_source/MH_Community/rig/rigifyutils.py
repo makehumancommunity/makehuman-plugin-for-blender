db86fadd892782dfe3030011479eb99f805f3770
Joel Palmius <joepal1976@hotmail.com>
2020-07-29 17:38:02 +0200

    All but face fitted

--- blender_source/MH_Community/rig/rigifyutils.py
+++ blender_source/MH_Community/rig/rigifyutils.py
@@ -3,6 +3,8 @@ from ..util import bl28
 from pprint import pprint
 
 _SIDEDMATCHES = dict()
+
+# Arm
 _SIDEDMATCHES["shoulder.#"] = dict()
 _SIDEDMATCHES["shoulder.#"]["head"] = "joint-#-clavicle"
 _SIDEDMATCHES["shoulder.#"]["tail"] = "joint-#-scapula"
@@ -135,36 +137,67 @@ _SIDEDMATCHES["toe.#"] = dict()
 _SIDEDMATCHES["toe.#"]["head"] = "joint-#-foot-1"
 _SIDEDMATCHES["toe.#"]["tail"] = "joint-#-foot-2"
 
+# Eye
+
+_SIDEDMATCHES["eye.#"] = dict()
+_SIDEDMATCHES["eye.#"]["head"] = "joint-#-eye"
+_SIDEDMATCHES["eye.#"]["tail"] = "joint-#-eye-target"
+
+
 
 _BONEMATCHER = dict()
 
+# Spine
 _BONEMATCHER["spine"] = dict()
 _BONEMATCHER["spine"]["head"] = "joint-pelvis"
 _BONEMATCHER["spine"]["tail"] = "joint-spine-4"
-
 _BONEMATCHER["spine.001"] = dict()
 _BONEMATCHER["spine.001"]["head"] = "joint-spine-4"
 _BONEMATCHER["spine.001"]["tail"] = "joint-spine-3"
-
 _BONEMATCHER["spine.002"] = dict()
 _BONEMATCHER["spine.002"]["head"] = "joint-spine-3"
 _BONEMATCHER["spine.002"]["tail"] = "joint-spine-2"
-
 _BONEMATCHER["spine.003"] = dict()
 _BONEMATCHER["spine.003"]["head"] = "joint-spine-2"
 _BONEMATCHER["spine.003"]["tail"] = "joint-spine-1"
-
 _BONEMATCHER["spine.004"] = dict()
 _BONEMATCHER["spine.004"]["head"] = "joint-spine-1"
 _BONEMATCHER["spine.004"]["tail"] = "joint-neck"
-
 _BONEMATCHER["spine.005"] = dict()
 _BONEMATCHER["spine.005"]["head"] = "joint-neck"
 _BONEMATCHER["spine.005"]["tail"] = "joint-head"
-
 _BONEMATCHER["spine.006"] = dict()
 _BONEMATCHER["spine.006"]["head"] = "joint-head"
 _BONEMATCHER["spine.006"]["tail"] = "joint-head-2"
+
+# Face meta bone - same place but half the height of spine.006
+_BONEMATCHER["face"] = dict()
+_BONEMATCHER["face"]["head"] = "joint-head"
+_BONEMATCHER["face"]["tail"] = "joint-head | joint-head-2"
+
+# Heel rollers
+_BONEMATCHER["heel.02.R"] = dict()
+_BONEMATCHER["heel.02.R"]["head"] = 15455
+_BONEMATCHER["heel.02.R"]["tail"] = 15452
+_BONEMATCHER["heel.02.L"] = dict()
+_BONEMATCHER["heel.02.L"]["head"] = 16759
+_BONEMATCHER["heel.02.L"]["tail"] = 16756
+
+# Pelvis movers
+_BONEMATCHER["pelvis.R"] = dict()
+_BONEMATCHER["pelvis.R"]["head"] = "joint-pelvis"
+_BONEMATCHER["pelvis.R"]["tail"] = 4284
+_BONEMATCHER["pelvis.L"] = dict()
+_BONEMATCHER["pelvis.L"]["head"] = "joint-pelvis"
+_BONEMATCHER["pelvis.L"]["tail"] = 10914
+
+# Breast
+_BONEMATCHER["breast.R"] = dict()
+_BONEMATCHER["breast.R"]["head"] = 3956
+_BONEMATCHER["breast.R"]["tail"] = 1764
+_BONEMATCHER["breast.L"] = dict()
+_BONEMATCHER["breast.L"]["head"] = 10619
+_BONEMATCHER["breast.L"]["tail"] = 8436
 
 for side in ['l', 'r']:
     sideUp = side.upper()    
@@ -180,7 +213,8 @@ class RigifyUtils:
     
     def __init__(self, obj):
         self._vgNameToIndex = dict()
-        self._vgIndexToName = dict()        
+        self._vgIndexToName = dict()   
+        self._vertCoords = dict()     
         self._vgVerts = dict();
         self._vgMeans = dict();
         
@@ -202,6 +236,7 @@ class RigifyUtils:
                 self._vgMeans[vg.name] = None
                 
         for v in self._obj.data.vertices:
+            self._vertCoords[v.index] = v.co
             for vg in v.groups:
                 if (vg.group) in self._vgVerts:
                     self._vgVerts[vg.group].append(v.co)
@@ -240,13 +275,16 @@ class RigifyUtils:
     def _getPositionFromMatch(self, boneName, part): 
         match = _BONEMATCHER[boneName]
         matchSub = match[part]        
-        if "|" in matchSub:
-            (left, right) = str(matchSub).split("|", 2)
-            left = str(left).strip()
-            right = str(right).strip()
-            position = self._average([self._vgMeans[left], self._vgMeans[right]])
-        else:                    
-            position = self._vgMeans[matchSub]
+        if str(matchSub).isnumeric():
+            position = self._vertCoords[int(matchSub)]
+        else:
+            if "|" in matchSub:
+                (left, right) = str(matchSub).split("|", 2)
+                left = str(left).strip()
+                right = str(right).strip()
+                position = self._average([self._vgMeans[left], self._vgMeans[right]])        
+            else:                    
+                position = self._vgMeans[matchSub]
         return position
     
     def _createRig(self):
